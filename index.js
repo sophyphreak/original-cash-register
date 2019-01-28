1d6ff703281e331feec734e53a6508b5ae6354dc
Andrew <arhorn@smcm.edu>
2019-01-28 22:37:32 +0700

    fixed spelling mistake, took out console.log(checkCashRegister());

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const checkCashRegister = (price, cash, cid) => {
   const cashRegisterClosedObject = makeCashRegisterClosedObject(cid);
   if (cashSimplyNotEnough(changeRequired, cid)) {
     // if the sum of cid is less than change required
-    return insufficentFundsObject;
+    return insufficientFundsObject;
   }
   let cidObject = createCashObject(cid);
   const changeObject = moveDrawerToChange(changeRequired, cidObject);
@@ -44,19 +44,19 @@ const checkCashRegister = (price, cash, cid) => {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-console.log(
-  checkCashRegister(19.5, 20, [
-    ['PENNY', 1.01],
-    ['NICKEL', 2.05],
-    ['DIME', 3.1],
-    ['QUARTER', 4.25],
-    ['ONE', 90],
-    ['FIVE', 55],
-    ['TEN', 20],
-    ['TWENTY', 60],
-    ['ONE HUNDRED', 100]
-  ])
-);
+// console.log(
+//   checkCashRegister(3.26, 100, [
+//     ['PENNY', 1.01],
+//     ['NICKEL', 2.05],
+//     ['DIME', 3.1],
+//     ['QUARTER', 4.25],
+//     ['ONE', 90],
+//     ['FIVE', 55],
+//     ['TEN', 20],
+//     ['TWENTY', 60],
+//     ['ONE HUNDRED', 100]
+//   ])
+// );
 
 module.exports = {
   checkCashRegister
