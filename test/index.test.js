38bd255c12940af783bbc8ee04b8d395d641bac4
Andrew <arhorn@smcm.edu>
2019-01-28 22:21:13 +0700

    made some tests for main function

--- test/index.test.js
+++ test/index.test.js
@@ -17,4 +17,43 @@ describe('checkCashRegister()', () => {
     ]);
     expect(result).to.be.an('object');
   });
+  it('should return correct object with $.50 in quarters', () => {
+    const result = checkCashRegister(19.5, 20, [
+      ['PENNY', 1.01],
+      ['NICKEL', 2.05],
+      ['DIME', 3.1],
+      ['QUARTER', 4.25],
+      ['ONE', 90],
+      ['FIVE', 55],
+      ['TEN', 20],
+      ['TWENTY', 60],
+      ['ONE HUNDRED', 100]
+    ]);
+    expect(result).to.eql({ status: 'OPEN', change: [['QUARTER', 0.5]] });
+  });
+  it('should return correct object with $60 in twenties, etc.', () => {
+    const result = checkCashRegister(3.26, 100, [
+      ['PENNY', 1.01],
+      ['NICKEL', 2.05],
+      ['DIME', 3.1],
+      ['QUARTER', 4.25],
+      ['ONE', 90],
+      ['FIVE', 55],
+      ['TEN', 20],
+      ['TWENTY', 60],
+      ['ONE HUNDRED', 100]
+    ]);
+    expect(result).to.eql({
+      status: 'OPEN',
+      change: [
+        ['TWENTY', 60],
+        ['TEN', 20],
+        ['FIVE', 15],
+        ['ONE', 1],
+        ['QUARTER', 0.5],
+        ['DIME', 0.2],
+        ['PENNY', 0.04]
+      ]
+    });
+  });
 });
