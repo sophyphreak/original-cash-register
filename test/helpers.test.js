39c29a2c67c89d1062cfcb7facb2094d76366844
Andrew <arhorn@smcm.edu>
2019-01-28 21:22:48 +0700

    added test for makeCashRegisterClosedObject()

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -2,7 +2,8 @@ const expect = require('chai').expect;
 
 const {
   cashSimplyNotEnough,
-  makeInsufficientFundsObject
+  makeInsufficientFundsObject,
+  makeCashRegisterClosedObject
 } = require('../helpers/helpers');
 
 const cid = [
@@ -28,12 +29,43 @@ describe('cashSimplyNotEnough()', () => {
   });
 });
 
-describe('makeInsufficientFundsObject', () => {
+describe('makeInsufficientFundsObject()', () => {
   it('should return an insufficient funds object', () => {
     const insufficientFunds = makeInsufficientFundsObject();
     expect(insufficientFunds).to.eql({
       status: 'INSUFFICIENT_FUNDS',
       change: []
+    });
+  });
+});
+
+describe('makeCashRegisterClosedObject()', () => {
+  it('should return a closed status object with correct change', () => {
+    const cid = [
+      ['PENNY', 1.01],
+      ['NICKEL', 2.05],
+      ['DIME', 3.1],
+      ['QUARTER', 4.25],
+      ['ONE', 90],
+      ['FIVE', 55],
+      ['TEN', 20],
+      ['TWENTY', 60],
+      ['ONE HUNDRED', 100]
+    ];
+    const closedWithCorrectChange = makeCashRegisterClosedObject(cid);
+    expect(closedWithCorrectChange).to.eql({
+      status: 'CLOSED',
+      change: [
+        ['PENNY', 1.01],
+        ['NICKEL', 2.05],
+        ['DIME', 3.1],
+        ['QUARTER', 4.25],
+        ['ONE', 90],
+        ['FIVE', 55],
+        ['TEN', 20],
+        ['TWENTY', 60],
+        ['ONE HUNDRED', 100]
+      ]
     });
   });
 });
